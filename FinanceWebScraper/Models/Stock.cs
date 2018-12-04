d31cd20b38f4d200ffcef9caa5cf50e7170b4215
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-04 06:15:50 -0500

    Stock.cs model class created with the required fields

---
+++ FinanceWebScraper/Models/Stock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceWebScraper.Models
+{
+    public class Stock
+    {
+        public int ID { get; set; }
+        public string Symbol { get; set; }
+        public decimal LastPrice { get; set; }
+        public decimal Change { get; set; }
+        public decimal PercentChange { get; set; }
+        public string Currency { get; set; }
+        public DateTime MarketTime { get; set; }
+        public decimal MarketCap { get; set; }
+    }
+}
